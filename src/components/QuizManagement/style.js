cc4cbcee89a1a1879e916cb225da938ee47e9ac1
JANFAN-L <janfanlu@gmail.com>
2021-10-29 10:11:38 -0400

    10/29

---
+++ src/components/QuizManagement/style.js
@@ -0,0 +1,39 @@
+import { createUseStyles } from 'react-jss'
+const flexCenter = { display: 'flex', justifyContent: 'center', alignItems: 'center' }
+export const useStyles = createUseStyles({
+    // homeContainer: {
+    //     display: "grid",
+    //     gridTemplateColumns: "15% 40% 25% 15% auto",
+    //     gridTemplateRows: "auto",
+    //     gridTemplateAreas:`
+    //     "header header header header header"
+    //     "sidebar main main main ."
+    //     `
+    // },
+
+    homeContainer: {
+        // display: "grid",
+        // gridTemplateColumns: "20vw auto",
+        // gridTemplateRows: "auto",
+        // gridTemplateAreas:`
+        // "header header"
+        // ". main"
+        // `
+        marginTop: "50px"
+    },
+
+    header: {
+        gridArea: "header"
+    },
+
+    main: {
+        // gridArea: "main",
+        // gridColumnStart: "1",
+        // paddingLeft: "20vw"
+    },
+
+    sidebar: {
+        gridArea: "sidebar",
+    }
+
+})
