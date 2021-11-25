d668167e213cac7e080edc570ef70bccfc5f5b2c
Andy Zheng <xinyao.zheng@stonybrook.edu>
2021-11-25 15:57:03 -0500

    connected the front end api to back end

--- src/components/UserProfile/About/index.js
+++ src/components/UserProfile/About/index.js
@@ -1,12 +1,36 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import userApis from '../../../api/user-api'
 import { useParams } from 'react-router-dom'
 import BODY from '../../../constant/body'
+import HEADER from '../../../constant/header'
 import { useStyles } from './style'
+import AuthContext from '../../../context/auth-context'
 const About = ()=>{
+    //sets the character limit in the about me page
+    const CHARACTER_LIMIT = 500
+
+    const { auth } = useContext(AuthContext)
     const { userId } = useParams()
-    const [about, setAbout] = useState("")
+    
+    const [isEditing, setIsEditing] = useState(false)
     const classes = useStyles()
+    
+    const [about, setAbout] = useState("")
+
+    const updateAboutMe = async ()=>{
+        const token = await auth.user.getIdToken()
+        let headers = {
+            [HEADER.TOKEN]: token
+        }
+        let config = {
+            headers: headers
+        }
+        let payload = {
+            [BODY.USERDESCRIPTION]: "I LIKE CHEESE AND FOOD"
+        }
+        setAbout("I LIKE CHEESE AND FOOD")
+        await userApis.setUserDescription(payload, config)
+    }
     useEffect(()=>{
         const loadAbout = async ()=>{
             try{
@@ -21,7 +45,7 @@ const About = ()=>{
     },[userId])
     return(
         <div>
-            <div>About Me!</div>
+            <div onClick={()=>updateAboutMe()}>About Me!</div>
             <div className={classes.aboutContainer}>{about}</div>
         </div>
     )
