a744421d0104e197375067c8e5990445d0a940d7
kai <kaiwendeveloer78@gmail.com>
2021-11-25 13:22:45 -0500

    add pagination for subscription list on home page and adjust pagination on category page

--- src/components/Home/SideBar/index.js
+++ src/components/Home/SideBar/index.js
@@ -12,10 +12,13 @@ import Watch_Later_sign from '../../../images/watchLater.png'
 import Like_Quizzes_sign from '../../../images/LikeQuizzes.png'
 import Community_sign from '../../../images/Community.png'
 import { useHistory } from 'react-router-dom'
+import { Button } from  "@mui/material"
+import LOCAL_CONSTANT from '../../../constant/local-storage';
 const SideBar = () => {
     const classes = useStyles()
     const [subscriptions, setSubscriptions] = useState([])
     const { auth } = useContext(AuthContext)
+    const [end, setEnd] = useState(false)
     const history = useHistory()
     useEffect(()=>{
         const loadSubscriptions = async (userId)=>{
@@ -24,14 +27,33 @@ const SideBar = () => {
                 [HEADER.TOKEN] : token
             }
             let response = await userApis.getSubscriptions(headers)
-            setSubscriptions(response.data)
+            updateSubscription(response)
         }
 
         if(auth.user!=null){
             loadSubscriptions()
         }
+ 
     }, [auth.user])
 
+    const updateSubscription = (response) => {
+        let sub_arr = response.data
+        if(response.data.length>0){
+            if(response.data.length == 11){
+                localStorage.setItem(LOCAL_CONSTANT.LAST_SUBSCRIBE_ID, response.data[response.data.length-1][BODY.SUBSCRIBEID])
+                localStorage.setItem(LOCAL_CONSTANT.LAST_SUBSCRIBE_DATE, response.data[response.data.length-1][BODY.CREATIONTIME])
+                sub_arr.pop()
+            }
+            else{
+                setEnd(true)
+            }
+            let new_sub = [...subscriptions]
+            for(var i = 0; i < sub_arr.length; i++){
+                new_sub.push(sub_arr[i])
+            }
+            setSubscriptions(new_sub)
+        }
+    }
     const redirectToHome = ()=>{
         console.log("redtriect home")
         history.push('/')
@@ -55,49 +77,68 @@ const SideBar = () => {
         history.push('/setting')
     }
 
-
+    const getMore = async ()=>{
+        const last_subscription_id = localStorage.getItem(LOCAL_CONSTANT.LAST_SUBSCRIBE_ID)
+        if(last_subscription_id!=null){
+            const token = await auth.user.getIdToken()
+            let headers = {
+                [HEADER.TOKEN] : token
+            }
+            let payload = {
+                [BODY.SUBSCRIBEID]: last_subscription_id
+            }
+            let response = await userApis.getMoreSubscriptionsById(payload, headers)
+            updateSubscription(response)
+        }
+    }
     return (
-        <div className={classes.sideBarContainer}>
-            <div className={classes.sideBarSelection}>
-                <div onClick={redirectToHome} className={classes.sideBarSelect}>
-                    <img className={classes.image_setting} src={Home_Sign} alt=""/>
-                    &nbsp;&nbsp;Home
+        <div className={classes.container}>
+            <div className={classes.sideBarContainer}>
+                <div className={classes.sideBarSelection}>
+                    <div onClick={redirectToHome} className={classes.sideBarSelect}>
+                        <img className={classes.image_setting} src={Home_Sign} alt=""/>
+                        &nbsp;&nbsp;Home
+                    </div>
+                    <div onClick={redirectToSubscription} className={classes.sideBarSelect}>
+                        <img className={classes.image_setting} src={Subcription_sign} alt=""/>
+                        &nbsp;&nbsp;Subscription
+                    </div>
+                    {/* <div className={classes.sideBarSelect}>
+                        <img className={classes.image_setting} src={Channel_sign} alt=""/>
+                        &nbsp;&nbsp;Channel
+                    </div> */}
+                    <div onClick={redirectTakeLater} className={classes.sideBarSelect}>
+                    <img className={classes.image_setting} src={Watch_Later_sign} alt=""/>
+                        &nbsp;&nbsp;Take Later
+                    </div>
+                    <div onClick={redirectLikeQuizzes} className={classes.sideBarSelect}>
+                    <img className={classes.image_setting} src={Like_Quizzes_sign} alt=""/>
+                        &nbsp;&nbsp;Like Quizzes
+                    </div>
+                    {/* <div className={classes.sideBarSelect}>
+                    <img className={classes.image_setting} src={Community_sign} alt=""/>
+                        &nbsp;&nbsp;Community
+                    </div> */}
                 </div>
-                <div onClick={redirectToSubscription} className={classes.sideBarSelect}>
-                    <img className={classes.image_setting} src={Subcription_sign} alt=""/>
-                    &nbsp;&nbsp;Subscription
-                </div>
-                {/* <div className={classes.sideBarSelect}>
-                    <img className={classes.image_setting} src={Channel_sign} alt=""/>
-                    &nbsp;&nbsp;Channel
-                </div> */}
-                <div onClick={redirectTakeLater} className={classes.sideBarSelect}>
-                <img className={classes.image_setting} src={Watch_Later_sign} alt=""/>
-                    &nbsp;&nbsp;Take Later
-                </div>
-                <div onClick={redirectLikeQuizzes} className={classes.sideBarSelect}>
-                <img className={classes.image_setting} src={Like_Quizzes_sign} alt=""/>
-                    &nbsp;&nbsp;Like Quizzes
-                </div>
-                {/* <div className={classes.sideBarSelect}>
-                <img className={classes.image_setting} src={Community_sign} alt=""/>
-                    &nbsp;&nbsp;Community
-                </div> */}
-            </div>
 
-            <div className={`${classes.subscriptionTop} ${classes.sideBarSelection}`}>
-                Subscriptions:
-                <div>
-                    {subscriptions.map((sub, i)=>{
-                        return <UserCard key={i}
-                                    subscribeTo={sub[BODY.SUBSCRIBETO]}/>
-                    })}
-                </div>
-            </div>
+                <div className={`${classes.subscriptionTop} ${classes.sideBarSelection}`}>
+                    Subscriptions:
+                    <div>
+                        {subscriptions.map((sub, i)=>{
+                            return <UserCard key={i}
+                                        subscribeTo={sub[BODY.SUBSCRIBETO]}/>
+                        })}
 
-            <div>
-                <div onClick={redirectToSetting} className={classes.sideBarSelect}>
-                    &nbsp;&nbsp;Settings
+                        {   end?<div>Thta's the end</div>:
+                            <Button onClick={getMore}>More</Button>
+                        }
+                    </div>
+                </div>
+
+                <div className={classes.settingBar}>
+                    <div onClick={redirectToSetting} className={classes.sideBarSelect}>
+                        &nbsp;&nbsp;Settings
+                    </div>
                 </div>
             </div>
         </div>
