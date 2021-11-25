5449d9fee8936bb9712776500bcce65be1e1583a
Andy Zheng <xinyao.zheng@stonybrook.edu>
2021-11-25 16:42:51 -0500

    basic editing works

--- src/components/UserProfile/About/index.js
+++ src/components/UserProfile/About/index.js
@@ -5,19 +5,34 @@ import BODY from '../../../constant/body'
 import HEADER from '../../../constant/header'
 import { useStyles } from './style'
 import AuthContext from '../../../context/auth-context'
-const About = ()=>{
+const About = () => {
     //sets the character limit in the about me page
     const CHARACTER_LIMIT = 500
 
     const { auth } = useContext(AuthContext)
     const { userId } = useParams()
-    
+
     const [isEditing, setIsEditing] = useState(false)
     const classes = useStyles()
-    
-    const [about, setAbout] = useState("")
 
-    const updateAboutMe = async ()=>{
+    const [about, setAbout] = useState("")
+    const [editAbout, setEditAbout] = useState("")
+
+    useEffect(() => {
+        const loadAbout = async () => {
+            try {
+                let response = await userApis.getUserDescription(userId)
+                setAbout(response.data[BODY.USERDESCRIPTION])
+            } catch (e) {
+
+            }
+        }
+
+        loadAbout()
+    }, [userId])
+
+
+    const updateAboutMe = async () => {
         const token = await auth.user.getIdToken()
         let headers = {
             [HEADER.TOKEN]: token
@@ -26,27 +41,28 @@ const About = ()=>{
             headers: headers
         }
         let payload = {
-            [BODY.USERDESCRIPTION]: "I LIKE CHEESE AND FOOD"
+            [BODY.USERDESCRIPTION]: editAbout
         }
-        setAbout("I LIKE CHEESE AND FOOD")
+        setAbout(editAbout)
         await userApis.setUserDescription(payload, config)
     }
-    useEffect(()=>{
-        const loadAbout = async ()=>{
-            try{
-                let response = await userApis.getUserDescription(userId)
-                setAbout(response.data[BODY.USERDESCRIPTION])
-            }catch(e){
-    
-            }
-        }
 
-        loadAbout()
-    },[userId])
-    return(
+    const handleSubmit = async () => {
+        updateAboutMe()
+        setIsEditing(false)
+    }
+
+    return (
         <div>
-            <div onClick={()=>updateAboutMe()}>About Me!</div>
-            <div className={classes.aboutContainer}>{about}</div>
+            <div onClick={() => updateAboutMe()}>About Me!</div>
+            {isEditing ?
+                <input onChange={e => setEditAbout(e.target.value)} />
+                :
+                <div className={classes.aboutContainer}>{about}</div>
+            }
+
+            <button onClick={() => setIsEditing(!isEditing)}>{isEditing ? "cancel" : "edit"}</button>
+            <button onClick={() => handleSubmit()}>submit</button>
         </div>
     )
 }
