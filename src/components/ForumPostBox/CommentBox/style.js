724835f768c4acd8382c46f97a82a6c28ca64524
JANFAN-L <janfanlu@gmail.com>
2021-12-03 23:27:55 -0500

    CSSEdit ForumComment

--- src/components/ForumPostBox/CommentBox/style.js
+++ src/components/ForumPostBox/CommentBox/style.js
@@ -3,13 +3,19 @@ import { createUseStyles } from 'react-jss'
 export const useStyles = createUseStyles({
     container: {
         marginTop: "1%",
+        //marginLeft:"5%",
+        width:"50vw",
         minHeight: "10vh",
-        display: "flex"
+        display: "flex",
+        backgroundColor:"#E6E6E6"
     },
 
     commentContainer: {
         // marginLeft: "10%"
-        marginRight: "auto"
+        marginRight: "auto",
+        width:"36vw",
+        textAlign:"left",
+        overflowWrap:"break-word",
     },
 
     userbox:{
@@ -20,5 +26,13 @@ export const useStyles = createUseStyles({
         whiteSpace: "nowrap",
         overflow: "hidden",
         width: "80%"
+    },
+    imgSize:{
+        width:"30px",
+        height: "30px",
+        borderRadius: "50%",
+    },
+    commentTittleContainer:{
+        marginLeft:"5px"
     }
 })
