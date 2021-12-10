76668e9437795ce752dc01e50cb09f50213b88eb
JANFAN-L <janfanlu@gmail.com>
2021-12-09 21:50:33 -0500

    CSSEdit Quiz Taking

--- src/components/QuizTaking/QuestionCard/style.js
+++ src/components/QuizTaking/QuestionCard/style.js
@@ -2,19 +2,22 @@ import { createUseStyles } from "react-jss";
 
 export const userStyles = createUseStyles({
     questionCardContainer: {
-        width: '30vw',
+        width: '31vw',
         textAlign: 'left',
         color: 'white',
-        backgroundColor: '#292b2c',
-        borderRadius: "10px"
+        backgroundColor: '#FFFFFF',
+        borderRadius: "10px",
+        marginTop:"60px"
     },
 
     titleFont: {
-        fontSize: '50px'
+        fontSize: '35px',
+        color:"#000000",
     },
 
     quizTextFont: {
-        fontSize: '25px'
+        fontSize: '25px',
+        color:"#000000",
     },
 
     buttonMargin: {
@@ -25,7 +28,32 @@ export const userStyles = createUseStyles({
         fontWeight: 'bold',
         fontFamily: 'Arial',
         borderRadius: '20px',
+        color:"black",
+        backgroundColor:"#EBEBEB",
     },
+    nextButton:{
+        width: '28vw',
+        marginLeft: '2px',
+        fontSize: '20px',
+        marginTop: `10px`,
+        fontWeight: 'bold',
+        fontFamily: 'Arial',
+        borderRadius: '20px',
+        color:"white",
+        backgroundColor:"#417CA2",
+    },
+    previousButton:{
+        width: '28vw',
+        marginLeft: '2px',
+        fontSize: '20px',
+        marginTop: `10px`,
+        fontWeight: 'bold',
+        fontFamily: 'Arial',
+        borderRadius: '20px',
+        color:"white",
+        backgroundColor:"#9E9E9E",
+    },
+
 
     image: {
         maxHeight: '300px'
