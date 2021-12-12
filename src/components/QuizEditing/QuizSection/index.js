0611d434175d1147b6e0a15de8654ab7b1cfa003
Andy Zheng <xinyao.zheng@stonybrook.edu>
2021-12-12 15:23:19 -0500

    style updated on the quiz editing page

--- src/components/QuizEditing/QuizSection/index.js
+++ src/components/QuizEditing/QuizSection/index.js
@@ -10,7 +10,7 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 // import Button from '@mui/material/Button'
 import { QUIZ_CATEGORY_NAME, QUIZ_CATEGORY_DICT } from '../../../constant/quiz-category'
-const QuizSection = ({quiz})=>{
+const QuizSection = ({ quiz }) => {
     const classes = useStyles()
     const imgRef = useRef()
     const [introduction, setIntroduction] = useState()
@@ -22,29 +22,29 @@ const QuizSection = ({quiz})=>{
     const history = useHistory()
     const [category, setCategory] = useState()
     const [categoryList, setCategoryList] = useState()
-    const onImageUpload = (event)=>{
+    const onImageUpload = (event) => {
         setImage(URL.createObjectURL(event.target.files[0]))
         setImageAsFile(event.target.files[0])
     }
-    const onChangeName = (event)=>{
+    const onChangeName = (event) => {
         setName(event.target.value)
     }
-    const clickUpload = ()=>{
+    const clickUpload = () => {
         imgRef.current.click()
     }
-    const onChangeIntroduction = (event)=>{
+    const onChangeIntroduction = (event) => {
         setIntroduction(event.target.value)
     }
-    const onChangeTimeLimit = (event)=>{
+    const onChangeTimeLimit = (event) => {
         setTimeLimit(event.target.value)
     }
-    const onAutoChange = (event, value)=>{
-        if(value!=null){
+    const onAutoChange = (event, value) => {
+        if (value != null) {
             setCategory(value['value'])
         }
     }
 
-    const onSave = async ()=>{
+    const onSave = async () => {
         let quizId = quiz[BODY.QUIZID]
         const payload = {
             [BODY.QUIZID]: quizId,
@@ -56,11 +56,11 @@ const QuizSection = ({quiz})=>{
 
         const token = await auth.user.getIdToken()
         let headers = {
-            [HEADER.TOKEN] : token
+            [HEADER.TOKEN]: token
         }
 
         await quizApis.updateQuiz(payload, headers)
-        if(imageAsFile !== undefined){
+        if (imageAsFile !== undefined) {
             const data = new FormData()
             data.append(BODY.QUIZID, quizId)
             data.append(BODY.QUIZTHUMBNAIL, imageAsFile)
@@ -68,15 +68,15 @@ const QuizSection = ({quiz})=>{
         }
         history.push('/quizManagement')
     }
-    useEffect(()=>{
-        const loadImage = async ()=>{
+    useEffect(() => {
+        const loadImage = async () => {
             let response = await quizApis.getQuizThumbnail(quiz[BODY.QUIZID])
             setImage(response.data)
         }
 
         let categorylist = []
-        for(var key in QUIZ_CATEGORY_DICT){
-            categorylist.push({label: key, value: QUIZ_CATEGORY_DICT[key]})
+        for (var key in QUIZ_CATEGORY_DICT) {
+            categorylist.push({ label: key, value: QUIZ_CATEGORY_DICT[key] })
         }
         setCategoryList(categorylist)
         setName(quiz[BODY.QUIZNAME])
@@ -84,58 +84,61 @@ const QuizSection = ({quiz})=>{
         setCategory(quiz[BODY.QUIZCATEGORY])
         setIntroduction(quiz[BODY.QUIZDESCRIPTION])
         loadImage()
-    },[quiz])
+    }, [quiz])
 
 
-    return(
-        <div className={classes.container}>
-            <input type="file" name="image" id="image" alt="quiz thumbnail" ref={imgRef} onChange={onImageUpload} className={classes.imgTag}/>
-            <div className={classes.quizName}>
-                <div className={classes.subTitle}>Quiz Name</div>
-                <input defaultValue={quiz[BODY.QUIZNAME]} className={classes.quizNameField} onKeyUp={onChangeName}/>
-            </div>
-
-            <div className={classes.cover}>
-                <div className={classes.subTitle}>Cover</div>
-                <div className={`${classes.btn} ${classes.toCenter}`} onClick={clickUpload}>
-                    <div className={classes.btnText}>upload</div>
+    return (
+        <div className = {classes.centerContainer}>
+            <div className={classes.container}>
+                <input type="file" name="image" id="image" alt="quiz thumbnail" ref={imgRef} onChange={onImageUpload} className={classes.imgTag} />
+                <div className={classes.quizName}>
+                    <div className={classes.subTitle}>Quiz Name</div>
+                    <input defaultValue={quiz[BODY.QUIZNAME]} className={classes.quizNameField} onKeyUp={onChangeName} />
                 </div>
-                <div className={classes.imgContainer}>
-                    <img  className={classes.imgSize} src={image} alt="quiz thumbnail"/>
+                <div className={classes.divider}></div>
+                <div className={classes.cover}>
+                    <div className={classes.subTitle}>Cover</div>
+                    <div className={`${classes.btn} ${classes.toCenter}`} onClick={clickUpload}>
+                        <div className={classes.btnText}>upload</div>
+                    </div>
+                    <div className={classes.imgContainer}>
+                        <img className={classes.imgSize} src={image} alt="quiz thumbnail" />
+                    </div>
                 </div>
-            </div>
-
-            <div className={classes.introduction}>
-                <div className={classes.subTitle}>Introduction</div>
-                <textarea defaultValue={quiz[BODY.QUIZDESCRIPTION]} onKeyUp={onChangeIntroduction} className={classes.introductionBox}/>
-            </div>
-
-            <div className={classes.timeLimit}>
-                <div className={classes.subTitle}>Time Limit</div>
-                <div className={classes.flexBox}>
-                    <input defaultValue={quiz[BODY.TIMELIMIT]} onKeyUp={onChangeTimeLimit}/>
-                    <div className={classes.unit}>min</div>
+                <div className={classes.divider}></div>
+                <div className={classes.introduction}>
+                    <div className={classes.subTitle}>Introduction</div>
+                    <textarea defaultValue={quiz[BODY.QUIZDESCRIPTION]} onKeyUp={onChangeIntroduction} className={classes.introductionBox} />
                 </div>
-            </div>
+                <div className={classes.divider}></div>
+                <div className={classes.timeLimit}>
+                    <div className={classes.subTitle}>Time Limit</div>
+                    <div className={classes.flexBox}>
+                        <input defaultValue={quiz[BODY.TIMELIMIT]} onKeyUp={onChangeTimeLimit} />
+                        <div className={classes.unit}>min</div>
+                    </div>
+                </div>
+                <div className={classes.divider}></div>
+                <div className={classes.quizCategory}>
+                    <div className={classes.subTitle}>Quiz Category</div>
+                    {categoryList !== undefined ?
+                        <Autocomplete
+                            disablePortal
+                            defaultValue={{ label: QUIZ_CATEGORY_NAME[quiz[BODY.QUIZCATEGORY]], value: quiz[BODY.QUIZCATEGORY] }}
+                            id="combo-box-demo"
+                            options={categoryList}
+                            isOptionEqualToValue={(option, value) => option.value === value.value}
+                            sx={{ width: 300 }}
+                            onChange={onAutoChange}
+                            renderInput={(params) => <TextField {...params} label="Movie" />}
+                        /> : <div>loading</div>}
 
-            <div className={classes.quizCategory}>
-                <div className={classes.subTitle}>Quiz Category</div>
-                    {categoryList!==undefined?
-                    <Autocomplete
-                        disablePortal
-                        defaultValue={{ label: QUIZ_CATEGORY_NAME[quiz[BODY.QUIZCATEGORY]], value: quiz[BODY.QUIZCATEGORY]}}
-                        id="combo-box-demo"
-                        options={categoryList}
-                        isOptionEqualToValue={(option,value)=>option.value===value.value}
-                        sx={{ width: 300 }}
-                        onChange={onAutoChange}
-                        renderInput={(params) => <TextField {...params} label="Movie" />}
-                    />:<div>loading</div>}
-
-            </div>
-            <div>
-                <div className={`${classes.toRight} ${classes.btn}`} onClick={onSave}>
-                    <div className={classes.btnText}>Save</div>
+                </div>
+                <div className={classes.divider}></div>
+                <div>
+                    <div className={`${classes.toRight} ${classes.btn}`} onClick={onSave}>
+                        <div className={classes.btnText}>Save</div>
+                    </div>
                 </div>
             </div>
         </div>
